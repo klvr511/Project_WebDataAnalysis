8bc36d3d2b0d0bd4294db13549c941f3af3fa99c
klvr511 <76495429+klvr511@users.noreply.github.com>
2026-04-29 18:12:18 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,22 +1,14 @@
-from flask import Flask, render_template, request, redirect, url_for
-import sqlite3
+from flask import Flask, render_template, request
+import psycopg2
 import os
 
 app = Flask(__name__)
 
-def init_db():
-    conn = sqlite3.connect('database.db')
-    cursor = conn.cursor()
-    cursor.execute('''
-        CREATE TABLE IF NOT EXISTS suggestions (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            name TEXT NOT NULL,
-            email TEXT NOT NULL,
-            idea TEXT NOT NULL
-        )
-    ''')
-    conn.commit()
-    conn.close()
+DB_URL = "postgresql://postgres:Meshary511%40@db.difpnyzysgndmabnpyod.supabase.co:5432/postgres"
+
+def get_db_connection():
+    conn = psycopg2.connect(DB_URL)
+    return conn
 
 @app.route('/')
 def index():
@@ -33,14 +25,15 @@ def contact():
 @app.route('/dashboard')
 def dashboard():
     try:
-        conn = sqlite3.connect('database.db')
-        cursor = conn.cursor()
-        cursor.execute("SELECT * FROM suggestions")
-        data = cursor.fetchall()
+        conn = get_db_connection()
+        cur = conn.cursor()
+        cur.execute('SELECT * FROM suggestions ORDER BY id DESC;')
+        data = cur.fetchall()
+        cur.close()
         conn.close()
         return render_template('dashboard.html', suggestions=data)
     except Exception as e:
-        return f"حدث خطأ أثناء تحميل لوحة التحكم: {e}"
+        return f"حدث خطأ في قاعدة البيانات: {e}"
 
 @app.route('/submit', methods=['POST'])
 def submit():
@@ -50,18 +43,17 @@ def submit():
         idea = request.form.get('idea')
         
         try:
-            conn = sqlite3.connect('database.db')
-            cursor = conn.cursor()
-            cursor.execute('INSERT INTO suggestions (name, email, idea) VALUES (?, ?, ?)', 
-                           (name, email, idea))
+            conn = get_db_connection()
+            cur = conn.cursor()
+            cur.execute('INSERT INTO suggestions (name, email, idea) VALUES (%s, %s, %s)',
+                        (name, email, idea))
             conn.commit()
+            cur.close()
             conn.close()
             return "<h1>تم استلام فكرتك بنجاح! شكراً لك.</h1><a href='/'>العودة للرئيسية</a>"
         except Exception as e:
             return f"حدث خطأ أثناء حفظ البيانات: {e}"
 
 if __name__ == '__main__':
-    init_db()  
-    
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port)
